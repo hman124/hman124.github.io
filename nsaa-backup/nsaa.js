1dee052c4069353074c21c6008cd013b6c72675b
hman124 <66391679+hman124@users.noreply.github.com>
2020-06-09 14:00:25 -0500

    fixing the footer

--- nsaa-backup/nsaa.js
+++ nsaa-backup/nsaa.js
@@ -27,8 +27,3 @@ else if(window.location.href.split('=').pop() == 'scavengerlog'){
 main.innerHTML = "<h3>Slideshow</h3><h1>Scavengers Log</h1><hr><h3>Downloads:</h3><ul><li><a href=\"documents/NSAAScavengerLog.docx\" download>Word Document (<i>.docx</i>)</a></li><li><a href=\"documents/NSAAScavengerlog.pdf\" download>PDF Document (<i>.pdf</i>)</a></li></ul><h3>Links:</h3><ul><li><a href=\"https://docs.google.com/presentation/d/11ixTwPLNbrplpCl4thuiN9ebSdz3tLUumTXNDb5ECAM/edit#slide=id.p\">View Online (Google Docs)</a></li><li><a href=\"documents/NSAAScavengerlog.pdf\">View Online (PDF)</a></li></ul>";}
 else if(window.location.href.split('=').pop() == 'terms'){
 main.innerHTML = "<h3>Page</h3><h1>Terms</h1><hr><h4><i>This site (NSAA Backup Library) is provided as a public service and is intended for use by NSAA MEMBERS AND MEMBERS OF PARTNERING COMPANIES ONLY. At no time is it acceptable to redistribute, copy, or share any of the files or links on this site - Doing so will result in the violating member being fired from the NSAA. These rules are strict. If you are not a member of the NSAA, you must LEAVE THIS SITE IMMEDIATELY.</i></h4>";}
-function hi(){
-var article = document.getElementsByTagName('body')[0];
-if (article.scrollHeight > article.clientHeight) {
-alert("yes");
-}	}
