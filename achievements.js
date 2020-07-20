68a3482098d7f0f8fd52ca46f8ab2449cc135914
hman124 <hman124@users.noreply.github.com>
2020-07-20 16:57:58 -0500

    Update achievements.js

--- achievements.js
+++ achievements.js
@@ -1,18 +1,27 @@
+//Variables
 var hasAchievement = false, achieveId, storedAchieve = JSON.parse(window.localStorage.getItem("achievements")), achievePage = "https://hman124.ml/achievements";
-var achievements = {
 
+var achievements = {
+//Give a user an achievement
 "grantAchievement": (a) => {
+//Check to see if achievements exist on the client's device. If it doesn't, set it up
+if(window.localStorage.achievements === undefined){
+window.localStorage.setItem("achievements", "[" + a + "]");}
+//If it does, just update it.
+else{
 var updatedList = storedAchieve.push(a);
 window.localStorage.setItem("achievements", updatedList);
 if(window.location.href = achievePage){
 achievements.checkAchievements();
-}},
+}}},
 
+//Update the achievements page
 "checkAchievements": () => {
 for(var i = 0; i < storedAchieve.length; i++){
-achieveId = "achievement" + storedAchieve[i];
+achieveId = "achievement-" + storedAchieve[i];
 document.getElementById(achieveId).style.backgroundColor = "white";}},
 
+//Check To See if an achievement is set
 "hasAchievement": (a) => {
 for(var i = 0; i < storedAchieve.length; i++){
 if(storedAchieve[i] == a){
@@ -21,6 +30,8 @@ break;}}
 return hasAchievement;
 }};
 
+
+//Sample Script I used 
 /* if (typeof(Storage) !== "undefined") {
  // Store
   localStorage.setItem("achievements-" + a, "achieved");
