f48a9ef86ca73c5151e6aa0c8d09b2c083d92840
hman124 <hman124@users.noreply.github.com>
2020-07-20 20:25:18 -0500

    updated achievements.js, hopefully it works this time, I used some pretty robust methods

--- achievements.js
+++ achievements.js
@@ -1,37 +1,54 @@
 //Variables
-var hasAchievement = false, storedArchState = true, storedAchieve = ["a", "b", "c"], achieveId, achievePage = "https://hman124.ml/achievements";
+var hasAchievement = false,
+  storedAchieve, achieveId, achievePage = "https://hman124.ml/achievements";
+//Inserting placeholder text to prevent errors
 
-if(window.localStorage.getItem("achievements") != undefined){
-storedArchState = false;}
-//Inserting placeholder text to prevent errors 
+try {
+  var storedAchieve = JSON.parse(window.localStorage.getItem("achievements"));
+} catch {
+  var storedAchieve = false;
+}
 
 var achievements = {
-//Give a user an achievement
-"grantAchievement": (a) => {
-//Check to see if achievements exist on the client's device. If it doesn't, set it up
-if(window.localStorage.achievements === undefined){
-window.localStorage.setItem("achievements", "[" + a + "]");}
-//If it does, just update it.
-else{
-var updatedList = storedAchieve.push(a);
-window.localStorage.setItem("achievements", updatedList);
-if(window.location.href == achievePage){
-achievements.checkAchievements();
-}}},
+  //Give a user an achievement
+  "grantAchievement": (a) => {
 
-//Update the achievements page
-"checkAchievements": () => {
-for(var i = 0; i < storedAchieve.length; i++){
-achieveId = "achievement-" + storedAchieve[i];
-document.getElementById(achieveId).style.backgroundColor = "white";}},
 
-//Check To See if an achievement is set
-"hasAchievement": (a) => {
-if(storedArchState){
-for(var i = 0; i < storedAchieve.length; i++){
-if(storedAchieve[i] == a){
-hasAchievement = true;
-break;}}
-return hasAchievement;}
-else{
-return false;}}};
+    //Check If The Achievement has already been added
+    for (var i = 0; i < storedAchieve.length; i++) {
+      if (storedAchieve[i] == a) {
+        hasAchievement = false;
+        break;
+      }
+    }
+    if (hasAchievement) {
+      return;
+    }
+
+
+    //Check to see if achievements exist on the client's device. If it doesn't, set it up.
+    if (!storedAchieve) {
+      window.localStorage.setItem("achievements", "[" + a + "]");
+    }
+    //If it does, just update it.
+    else {
+      var updatedList = storedAchieve.push(a);
+      window.localStorage.setItem("achievements", updatedList);
+      if (window.location.href == achievePage) {
+        achievements.checkAchievements();
+      }
+    }
+  },
+
+  //Update the achievements page
+  "checkAchievements": () => {
+    if (storedAchieve) {
+      for (var i = 0; i < storedAchieve.length; i++) {
+        achieveId = "achievement-" + storedAchieve[i];
+        document.getElementById(achieveId).style.backgroundColor = "white";
+      }
+    } else {
+      return;
+    }
+  }
+};
