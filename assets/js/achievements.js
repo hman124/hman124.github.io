88bb64484543c05657c9b0e60f9874e9e8897838
hman124 <hman124@users.noreply.github.com>
2020-07-24 21:27:05 -0500

    updated most of the sites to ver 1.2

--- assets/js/achievements.js
+++ assets/js/achievements.js
@@ -1,7 +1,10 @@
 //Achievements.js - Harrison Steed - Designed And Built For Hman124.ml
 
 //Variables
-var hasAchievement = false, clickCount = 0, storedAchieve, achieveId, achievePage = "https://hman124.ml/achievements/", currentScript = document.currentScript;
+var hasAchievement = false,
+  clickCount = 0,
+  storedAchieve, achieveId, achievePage = "https://hman124.ml/achievements/",
+  currentScript = document.currentScript;
 //Inserting placeholder text to prevent errors
 
 try {
@@ -11,11 +14,11 @@ try {
 }
 
 var achievements = {
-//Give a user an achievement
+  //Give a user an achievement
   "grantAchievement": (a) => {
-if(currentScript === null){
-	return "Nice try";
-	document.currentScript.remove();}	
+    if (isValid(a)) {
+      return "Nice try";
+    }
     if (storedAchieve) {
       //Check If The Achievement has already been added
       for (var i = 0; i < storedAchieve.length; i++) {
@@ -30,8 +33,8 @@ if(currentScript === null){
 
 
       //Set The Local Storage.
-     storedAchieve.push(a);
-	 var stringList = storedAchieve.toString()
+      storedAchieve.push(a);
+      var stringList = storedAchieve.toString()
       window.localStorage.setItem("achievements", "[" + stringList + "]");
       if (window.location.href == achievePage) {
         achievements.checkAchievements();
@@ -39,7 +42,7 @@ if(currentScript === null){
     } else {
       //If the local storage var is not already set, create it.
       window.localStorage.setItem("achievements", "[" + a + "]");
-	   if (window.location.href == achievePage) {
+      if (window.location.href == achievePage) {
         achievements.checkAchievements();
       }
     }
@@ -60,30 +63,64 @@ if(currentScript === null){
 
 //12:00 PM Achievement
 var date = new Date();
-if(date.getHours == "7" /* && date.getMinutes == "00" */ ){
-	achievements.grantAchievement(4);}
+if (date.getHours() == "12" && date.getMinutes() == "00") {
+  achievements.grantAchievement(4);
+}
 
 //100x click achievement
 window.onclick = () => {
-	 clickCount++;
+  clickCount++;
   if (clickCount == 100) {
-      achievements.grantAchievement(1);
-    }
+    achievements.grantAchievement(1);
   }
-  
+}
+
 //Secret TextBox Concept
-//If A user Types A certian word, it will appear.
+//If a user Types a certian word, it will appear.
 
 const word = ["t", "e", "s", "t"];
 var wordIndex = 0;
 window.onkeypress = (event) => {
-	var key = event.key;
-if(word[wordIndex] == key.toLowerCase()){
-wordIndex++;
-if(wordIndex == word.length){
-	var cmdPrompt = document.createElement("TEXTAREA");
-document.getElementById("container").appendChild(cmdPrompt);
-}}
-else{
-if(!cmdPrompt){
-wordIndex = 0;}}}
+  var key = event.key;
+  if (word[wordIndex] == key.toLowerCase()) {
+    wordIndex++;
+    if (wordIndex == word.length) {
+      var cmdPrompt = document.createElement("TEXTAREA");
+      document.getElementById("container").appendChild(cmdPrompt);
+    }
+  } else {
+    if (!cmdPrompt) {
+      wordIndex = 0;
+    }
+  }
+}
+
+//Check function - validates all of the achievements to prevent false grents from the console
+const isValid = (achievement) => {
+  if (achievement == 1) {
+    if (clickCount == 100 || clickCount > 100) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (achievement == 2) {
+    if (window.location.href != "https://hman124.ml/" && window.location.href != "https://hman124.ml/achievements/") {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (achievement == 3) {
+    if (window.location.href == "https://hman124.ml/achievements/dns1892") {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (achievement == 4) {
+    var date = new date();
+    if (date.getHours() == "12" && date.getMinutes() == "00") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
